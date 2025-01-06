c44c3a4358df661b762b4d96b1ca6b88909a6288
pusty1830 <126335408+pusty1830@users.noreply.github.com>
2025-01-06 09:17:37 +0530

    .

--- src/services/axiosClient.ts
+++ src/services/axiosClient.ts
@@ -3,7 +3,9 @@ import { jwtDecode } from "jwt-decode";
 
 // const BASE_URL = 'http://localhost:8080/api/'
 
-const BASE_URL = 'http://3.105.187.169:8080/api/'
+// const BASE_URL = 'http://3.105.187.169:8080/api/'
+
+const BASE_URL="https://livabhi-server.vercel.app/api"
 
 
 export function getCurrentAccessToken() {
